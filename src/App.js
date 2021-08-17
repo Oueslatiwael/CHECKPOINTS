5bcda50bc173f2bb1c4254b09e282657c848043a
Oueslatiwael <Oueslatiwael17@gmail.com>
2021-08-18 00:54:07 +0100

    react project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from "./componants/MyForm.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar from "./componants/MyNavBar.js";
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <h1>REGISTRATION FORM</h1>
+      <MyForm />
+
+        
+    </div>
+  );
+}
+
+export default App;
